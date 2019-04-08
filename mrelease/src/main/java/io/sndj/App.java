57adc9484212c474efc925bfbe5edde4b80c1727
yuechao <yuechao@hualala.com>
2019-04-08 11:16:33 +0800

    mrelease

---
+++ mrelease/src/main/java/io/sndj/App.java
@@ -0,0 +1,13 @@
+package io.sndj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
